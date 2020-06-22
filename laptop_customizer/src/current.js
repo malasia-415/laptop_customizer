0a5e16c4c2f40410656d43041821abe3a9ee1b43
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-06-22 00:32:15 -0700

    edits

--- laptop_customizer/src/current.js
+++ laptop_customizer/src/current.js
@@ -1,4 +1,14 @@
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+ import React from 'react';
+  import USCurrencyFormat from 'USCurrencyFormat';
+
+  class USCurrencyFormat extends React.Component {
+    render() {
+      return (
+        <USCurrencyFormat>
+          style: 'currency',
+          currency: 'USD'
+        </USCurrencyFormat>
+      );
+    };
+  }
+  export default USCurrencyFormat;

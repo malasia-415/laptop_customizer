b71de68adf7f12c7596325276881ba9b9643ed9f
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-06-22 00:32:51 -0700

    edits

--- laptop_customizer/src/slugify.js
+++ laptop_customizer/src/slugify.js
@@ -1,1 +1,12 @@
-import slugify from 'slugify';
+import Slugify from 'slugify';
+
+class Slugify extends React.Component {
+    render() {
+        return (
+            <Slugify>
+                <p>This is the slugify Component</p> 
+            </Slugify>
+        );
+    }
+}
+export default Slugify;

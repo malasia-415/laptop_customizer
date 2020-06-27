aea885893a78f73545e774951019621d3ad3b5fa
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-06-27 13:22:56 -0700

    Edits

--- laptop_customizer/src/summary.js
+++ laptop_customizer/src/summary.js
@@ -1,17 +1,19 @@
-import summary from 'summary';
+import slugify from "slugify";
 
-class summary extends React.Component {
-  render() {
-    return (
-      <div className="summary__option" key={featureHash}>
-        <div className="summary__option__label">{feature} </div>
-        <div className="summary__option__value">{selectedOption.name}</div>
-        <div className="summary__option__cost">
-          {USCurrencyFormat.format(selectedOption.cost)}
-        </div>
-      </div>
-    );
-  };
-} 
+// import summary from 'summary';
 
-export default summary;
+// class summary extends React.Component {
+//   render() {
+//     return (
+//       <div className="summary__option" key={featureHash}>
+//         <div className="summary__option__label">{feature} </div>
+//         <div className="summary__option__value">{selectedOption.name}</div>
+//         <div className="summary__option__cost">
+//           {USCurrencyFormat.format(selectedOption.cost)}
+//         </div>
+//       </div>
+//     );
+//   };
+// } 
+
+export default slugify;

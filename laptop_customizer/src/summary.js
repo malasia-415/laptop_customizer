2f07db676e72578f98ac677cd395020d0ac9e49e
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-06-30 22:38:43 -0700

    Clean edits

--- laptop_customizer/src/summary.js
+++ laptop_customizer/src/summary.js
@@ -1,18 +1,46 @@
-import summary from "summary";
+import React from 'react';
+// import USCurrencyFormat from './USCurrencyFormat';
+import Total from './Total';
+import SummaryOptions from './SummaryOptions';
 
-// import summary from 'summary';
+class Summary extends React.Component {
+    render() {
 
-// class summary extends React.Component {
-//   render() {
-//     return (
-//       <div className="summary__option" key={featureHash}>
-//         <div className="summary__option__label">{feature} </div>
-//         <div className="summary__option__value">{selectedOption.name}</div>
-//         <div className="summary__option__cost">
-//           {USCurrencyFormat.format(selectedOption.cost)}
-//         </div>
-//       </div>
-//     );
-//   };
-// } 
-export default summary;
+        const summary = Object.keys(this.props.selected).map((feature, idx) => {
+          const featureHash = feature + '-' + idx;
+          const selectedOption = this.props.selected[feature];
+    
+          return (
+            <SummaryOptions 
+              featureHash={featureHash}
+              feature={feature}
+              optionName={selectedOption.name}
+              optionCost={selectedOption.cost}
+              USCurrencyFormat={this.props.USCurrencyFormat}
+            /> 
+            // <div className="summary__option" key={featureHash}>
+            //   <div className="summary__option__label">{feature} </div>
+            //   <div className="summary__option__value">{selectedOption.name}</div>
+            //   <div className="summary__option__cost">
+            //     {this.props.USCurrencyFormat.format(selectedOption.cost)}
+            //   </div>
+            // </div>
+          );
+        });
+        return (
+            <section className="main__summary">
+                <h2>Your cart</h2>
+                {summary}
+                <Total 
+                  total={this.props.total}
+                  USCurrencyFormat={this.props.USCurrencyFormat}
+                />
+                </section>
+            );
+        }
+
+}
+
+
+
+export default Summary;
